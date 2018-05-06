ad6b736311ef4d594a83b18753926f5195f276dd
CP7TBL <39010908+CP7TBL@users.noreply.github.com>
2018-05-07 03:00:44 +0800

    Update lbot-2018.js

--- lbot-2018.js
+++ lbot-2018.js
@@ -68,11 +68,11 @@ document.addEventListener('DOMContentLoaded', function() {
           robot.chatHistory.push(img);
           robot.render();
         }],
-	['召喚峽谷', function() {
+	['造換峽谷', function() {
           img.src = 'https://goo.gl/68wMM8';
           robot.chatHistory.push(img);
           robot.render();
-        }]	    
+        }]
       );
     }});
 
