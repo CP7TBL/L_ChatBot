48a9bb1bf9ed99003e285c8fb92042e143293a6c
CP7TBL <39010908+CP7TBL@users.noreply.github.com>
2018-05-07 03:28:18 +0800

    Update lbot-2018.js

--- lbot-2018.js
+++ lbot-2018.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
   
  
  LBot.prototype.responses.push(
-    { name: 'pt', rule: /元素週期表/i, action: function(robot, msg) {
+    { name: 'pt', rule: /元素*|週期表/i, action: function(robot, msg) {
       var img = document.createElement('img');
       img.src = 'https://goo.gl/uH4atu';
       robot.chatHistory.push(img);
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }});
 	
   LBot.prototype.responses.push(
-    { name: 'tpmrt', rule: /捷運/i, action: function(robot, msg) {
+    { name: 'tpmrt', rule: /捷運*|/i, action: function(robot, msg) {
       var img = document.createElement('img');
       img.src = 'https://goo.gl/izHMzW';
       robot.chatHistory.push(img);
