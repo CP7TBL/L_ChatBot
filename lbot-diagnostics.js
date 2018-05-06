76d497bf7bc7997de27b2be2647449cd254a0f98
CP7TBL <39010908+CP7TBL@users.noreply.github.com>
2018-05-07 03:48:50 +0800

    Update lbot-diagnostics.js

--- lbot-diagnostics.js
+++ lbot-diagnostics.js
@@ -5,10 +5,17 @@ document.addEventListener('DOMContentLoaded', function() {
     { name: 'ping', rule: /PING$/i, action: function(robot, msg) {
       robot.send('PONG');
     }});
+  
+  
+  
+  // 回聲 使用者輸入 : ECHO XXXXX  XXXXX=任意訊息
   LBot.prototype.responses.push(
     { name: 'echo [string]', rule: /ECHO (.*)$/i, action: function(robot, msg) {
       robot.send(msg);
     }});
+  
+  
+  
   LBot.prototype.responses.push(
     { name: 'time', rule: /TIME$|時間/i, action: function(robot, msg) {
       robot.send('現在時間為 ' + new Date());
